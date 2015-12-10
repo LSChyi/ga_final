a9c2978a47d4e6108bcfec8f90af83227cd6e546
LSChyi <alan81920@gmail.com>
2015-12-11 02:11:29 +0800

    add some basic function into ga

--- youbikeGa.cpp
+++ youbikeGa.cpp
@@ -1,9 +1,23 @@
+#include <cstring>
 #include "youbikeGa.h"
 
-YoubikeGa::YoubikeGa(int ell, int max_gen, int station_id) {
+YoubikeGa::YoubikeGa(int n_ell, int n_max_gen, int n_station_id) {
     Chromosome ch(ell);
+    generator_model = new double[ell];
+    max_gen = n_max_gen;
+    ell = n_ell;
+    station_id = n_station_id;
+    generation = 0;
 }
 
 YoubikeGa::~YoubikeGa() {
+    if(generator_model != NULL)
+        delete []generator_model;
+}
 
+bool YoubikeGa::should_terminate() {
+    if(generation >= max_gen)
+        return true;
+    
+    return false;
 }
--- youbikeGa.h
+++ youbikeGa.h
@@ -7,10 +7,19 @@ class YoubikeGa {
 public:
     YoubikeGa(int n_ell, int n_max_gen, int n_station_id);
     ~YoubikeGa();
+    void run();
+    Chromosome result();
 
 private:
     bool should_terminate();
+    Chromosome generate_chromosome();
     void update_generator();
+
+    double *generator_model;
+    int max_gen;
+    int ell;
+    int station_id;
+    int generation;
 };
 
 #endif
