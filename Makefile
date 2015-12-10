8a860619972e859d64c31d65a22a9cd34fb1be07
LSChyi <alan81920@gmail.com>
2015-12-11 00:28:48 +0800

    build main file

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
+all: youbike_op
+	g++ -o bin/youbike_op bin/*.o
+
+youbike_op: main.cpp
+	g++ -c main.cpp -o bin/youbike_op.o
+
 clean:
-	rm -f *.o
+	rm -f bin/*.o
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if(argc != 6) {
+        cout << "parameters number not match!" << endl;
+        cout << "youbike_op ell stationId repeat maxGen display" << endl;
+
+        return -1;
+    }
+
+    int ell = atoi(argv[1]);
+    int station_id = atoi(argv[2]);
+    int repeat = atoi(argv[3]);
+    int max_gen = atoi(argv[4]);
+    bool display = (atoi(argv[5]) > 0) ? true : false ;
+
+    return 0;
+}
