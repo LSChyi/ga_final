e908c19e40209907a5a43c22d07c86ffd67e348c
andy3466 <andychen662000@gmail.com>
2015-12-17 21:00:22 +0800

    Update youbikeGa.cpp

--- youbikeGa.cpp
+++ youbikeGa.cpp
@@ -3,12 +3,15 @@
 #include "youbikeGa.h"
 using namespace std;
 
-YoubikeGa::YoubikeGa(int n_ell, int n_max_gen, int n_station_id) {
-    generator_model = new double[ell];
+YoubikeGa::YoubikeGa(int n_ell, int n_max_gen, int n_station_id, int n_park) {
+    generator_model = new double* [ell];
     for(int i = 0; i < n_ell; ++i) {
-        generator_model[i] = 0.5;
+		generator_model[i] = new double [n_park*2+1] ;  //  -n_park ~ 0 ~ +n_park
+		for(int i2 =0 ; i2 < n_park; i2++){
+            generator_model[i][i2] = 1.0/double(n_park*2+1);
+	    }
     }
-
+    park = n_park ;
     max_gen = n_max_gen;
     ell = n_ell;
     station_id = n_station_id;
@@ -21,17 +24,37 @@ YoubikeGa::~YoubikeGa() {
 }
 
 bool YoubikeGa::should_terminate() {
-    if(generation >= max_gen)
+	// generation >= max_gen
+    if(generation >= max_gen)   
         return true;
-
+    // all converge  , all > 0.999
+    int temp = 0; 
+    for (int i = 0 ; i < ell; i++){
+		 bool check = true;
+		 for (int i2 = 0 ;i2 < park ; i2++){
+		     if (generator_model[i][i2]>0.999){
+			     check = false;
+			     break;
+			 }    
+		  if(check)
+		     temp++;
+		 }			
+	}
+	if(temp == ell)
+	  return true;
+	  
     return false;
 }
 
 void YoubikeGa::run() {
     while(should_terminate()) {
         // 1. generate two chromosome
+        
         // 2. ask fitness function
+        
         // 3. update model
+        
         // 4. generation += 1
+        generation += 1; 
     }
 }
