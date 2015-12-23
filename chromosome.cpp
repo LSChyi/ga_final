22bf926b20b8962eda4400c0b4a02b53b479928d
andy3466 <andychen662000@gmail.com>
2015-12-23 22:04:18 +0800

    Update chromosome.cpp

--- chromosome.cpp
+++ chromosome.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstring>
 #include "chromosome.h"
+#include <time.h>
+
 using namespace std;
 
 Chromosome::Chromosome(int ell) {
@@ -25,4 +27,21 @@ void Chromosome::output() {
         cout << genes[i] << " ";
     }
     cout << "]" << endl;
+}
+
+double** Chromosome::sample(**double p_vec,int n_park) {
+	for(int i = 0 ;i < size; i++){
+		srand (time(NULL));
+		double dise = (rand()%(2*n_park+1))/(2*n_park+1);
+	    double temp = 0;
+		for(int i2 = 0 ;i2 < n_park*2+1; i2++){
+		    temp += p_vec[i][i2];
+		    if(temp > dise){
+		      genes[i]=i2-n_park;
+		      break;
+			}   
+	        if(i2 == 2*n_park)
+	          gense[i] = n_park ;
+		}
+	}
 }
