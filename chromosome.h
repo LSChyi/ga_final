a9aef46837b0fc66e6e10d054cdb48b174376be7
andy3466 <andychen662000@gmail.com>
2015-12-23 21:52:24 +0800

    Update chromosome.h

--- chromosome.h
+++ chromosome.h
@@ -7,7 +7,7 @@ public:
     ~Chromosome();
     void init(int values[]);
     void output();
-    double** sample(double**);
+    double** sample(double** p_vec,int n_park);
 private:
     int *genes;
     int size;
