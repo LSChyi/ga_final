fd3e77b910156d35dfb0b8beea961b6df2cc9090
andy3466 <andychen662000@gmail.com>
2016-01-03 18:16:04 +0800

    add argv[6] for population size

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    if(argc != 6) {
+    if(argc != 7) {
         cout << "parameters number not match!" << endl;
         cout << "youbikeOp ell stationId repeat maxGen display" << endl;
 
@@ -16,9 +16,9 @@ int main(int argc, char *argv[]) {
 	int repeat = atoi(argv[3]);
     int max_gen = atoi(argv[4]);
     bool display = (atoi(argv[5]) > 0) ? true : false ;
-    
+    int population_size = atoi(argv[6]);
     for(int i = 0; i < repeat; ++i) {
-        YoubikeGa ga(ell, max_gen, station_id);
+        YoubikeGa ga(ell, max_gen, station_id,population_size);
         ga.run();
     }
 
