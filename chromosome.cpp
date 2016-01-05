ee992383afc39c590239eb39a603b58dc305ef43
andy3466 <andychen662000@gmail.com>
2016-01-05 11:22:34 +0800

    Update chromosome.cpp

--- chromosome.cpp
+++ chromosome.cpp
@@ -29,21 +29,21 @@ void Chromosome::output() {
     }
     cout << "]" << endl;
 }
+void Chromosome::sample(double** p_vec,int n_park,int* origin) {
 
-int* Chromosome::sample(double** p_vec,int n_park) {
 	for(int i = 0 ;i < size; i++){
-	
-		double dise = double((rand()%(2*n_park+1)))/(2*n_park+1);
+		
+		int a = rand();
+		double dise = double((a%(2*n_park+1)))/(2*n_park+1);
 	    double temp = 0;
 		for(int i2 = 0 ;i2 < n_park*2+1; i2++){
 		    temp += p_vec[i][i2];
 		    if(temp > dise){
-		      genes[i]=i2-n_park;
+		      origin[i]=i2-n_park;
 		      break;
 			}   
 	        if(i2 == 2*n_park)
-	          genes[i] = n_park ;
+	          origin[i] = n_park ;
 		}
 	}
-	return genes;
 }
